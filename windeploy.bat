641030a274374201304b5764c98b6e7046ddc604
Sulfred Lee <sflee1112@gmail.com>
2017-07-22 07:56:15 +0800

    update all cmake configuration files

--- windeploy.bat
+++ windeploy.bat
@@ -1,2 +1,3 @@
-E:\WorkShop\Software_Dev\3rd_party\Qt5.6.0\5.6\msvc2013_64\bin\windeployqt.exe PcapReplayer.exe
-I:\WorkShop\Software_Dev\3rd_party\Qt5.6.0\5.6\msvc2013_64\bin\windeployqt.exe PcapReplayer.exe
+::E:\WorkShop\Software_Dev\3rd_party\Qt5.6.0\5.6\msvc2013_64\bin\windeployqt.exe PcapReplayer.exe
+::I:\WorkShop\Software_Dev\3rd_party\Qt5.6.0\5.6\msvc2013_64\bin\windeployqt.exe PcapReplayer.exe
+C:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\windeployqt.exe PcapReplayer.exe
